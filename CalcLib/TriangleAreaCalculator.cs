fa88abbcd6bd5cb298e6120631684a47add5cbff
Lestat Delionkur <Flister66@gmail.com>
2015-08-12 02:33:28 +0300

    Релиз

---
+++ CalcLib/TriangleAreaCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLib
+{
+    public class TriangleAreaCalculator
+    {
+        public double RightTriangleAreaCalc(double side1, double side2, double side3)
+        {
+            IList<double> parList = new List<double>() { side1, side2, side3 };
+
+            foreach (var side in parList)
+                if (side <= 0)
+                    throw new ArgumentOutOfRangeException("side" + parList.IndexOf(side)+1, "Negative or zero value");
+
+            var hype = parList.Max();
+            parList.Remove(hype);
+
+            if (hype != Math.Sqrt(parList.Select(x => x * x).Sum()))
+            {
+                throw new ArgumentException("Not right triangle value");
+            }
+            return 0.5 * parList[0] * parList[1];
+
+        }
+    }
+}
---
+++ CalcLibTests/TriangleAreaCalculatorTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcLib.Tests
+{
+    [TestClass()]
+    public class TriangleAreaCalculatorTests
+    {
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RightTriangleZeroTest()
+        {
+            TriangleAreaCalculator target = new TriangleAreaCalculator();
+            var area = target.RightTriangleAreaCalc(0, 1, 4);            
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void RightTriangleNegativeTest()
+        {
+            TriangleAreaCalculator target = new TriangleAreaCalculator();
+            var area = target.RightTriangleAreaCalc(-1, 1, 4);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RightTrianglePifagorTest()
+        {
+            TriangleAreaCalculator target = new TriangleAreaCalculator();
+            var area = target.RightTriangleAreaCalc(1, 1, 2);
+
+        }
+
+        [TestMethod()]
+        public void RightTriangleSuccessTest()
+        {
+            TriangleAreaCalculator target = new TriangleAreaCalculator();
+            var area = target.RightTriangleAreaCalc(3, 4, 5);
+
+            Assert.AreEqual(6, area);
+
+        }
+    }
+}
